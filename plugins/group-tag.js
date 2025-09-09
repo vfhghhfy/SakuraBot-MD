910bd8165901658013aca68e196a3a6c617e5da6
Ell Contrerax <argumedodie@gmail.com>
2025-09-09 12:05:44 -0500

    Crear group-tag.js

---
+++ plugins/group-tag.js
@@ -0,0 +1,41 @@
+import { generateWAMessageFromContent } from '@whiskeysockets/baileys'
+import * as fs from 'fs'
+
+var handler = async (m, { conn, text, participants, isOwner, isAdmin }) => {
+if (!m.quoted && !text) return conn.reply(m.chat, `🕸 Por favor, ingresa un texto o cita un mensaje.`, m)
+try { 
+let users = participants.map(u => conn.decodeJid(u.id))
+let q = m.quoted ? m.quoted : m || m.text || m.sender
+let c = m.quoted ? await m.getQuotedObj() : m.msg || m.text || m.sender
+let msg = conn.cMod(m.chat, generateWAMessageFromContent(m.chat, { [m.quoted ? q.mtype : 'extendedTextMessage']: m.quoted ? c.message[q.mtype] : { text: '' || c }}, { quoted: null, userJid: conn.user.id }), text || q.text, conn.user.jid, { mentions: users })
+await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
+} catch {    
+let users = participants.map(u => conn.decodeJid(u.id))
+let quoted = m.quoted ? m.quoted : m
+let mime = (quoted.msg || quoted).mimetype || ''
+let isMedia = /image|video|sticker|audio/.test(mime)
+let more = String.fromCharCode(8206)
+let masss = more.repeat(850)
+let htextos = `${text ? text : ''}`
+if ((isMedia && quoted.mtype === 'imageMessage') && htextos) {
+var mediax = await quoted.download?.()
+conn.sendMessage(m.chat, { image: mediax, mentions: users, caption: htextos, mentions: users }, { quoted: null })
+} else if ((isMedia && quoted.mtype === 'videoMessage') && htextos) {
+var mediax = await quoted.download?.()
+conn.sendMessage(m.chat, { video: mediax, mentions: users, mimetype: 'video/mp4', caption: htextos }, { quoted: null })
+} else if ((isMedia && quoted.mtype === 'audioMessage') && htextos) {
+var mediax = await quoted.download?.()
+conn.sendMessage(m.chat, { audio: mediax, mentions: users, mimetype: 'audio/mp4', fileName: `Hidetag.mp3` }, { quoted: null })
+} else if ((isMedia && quoted.mtype === 'stickerMessage') && htextos) {
+var mediax = await quoted.download?.()
+conn.sendMessage(m.chat, {sticker: mediax, mentions: users}, { quoted: null })
+} else {
+await conn.reply(m.chat, htextos, null, { mentions: [users] })
+}}}
+
+handler.help = ['hidetag', 'tag']
+handler.tags = ['group']
+handler.command = ['hidetag', 'tag']
+handler.admin = true
+
+export default handler
