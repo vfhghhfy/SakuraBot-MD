a9c6f8f6631edcafe7c9236983cd4f86279dd9ac
Ell Contrerax <argumedodie@gmail.com>
2025-09-09 11:54:55 -0500

    Crear jadibot-leave.js

---
+++ plugins/jadibot-leave.js
@@ -0,0 +1,26 @@
+let handler = async (m, { conn, args }) => {
+  const isSocketOwner = [
+    conn.user.jid,
+    ...(global.owner || []).map(n => n + '@s.whatsapp.net'),
+  ].includes(m.sender);
+
+  if (!isSocketOwner) {
+    return m.reply('🕸 Solo el propietario del socket puede usar este comando.');
+  }
+
+  const groupId = args[0] || m.chat;
+
+  try {
+    // await conn.sendMessage(m.chat, { text: `🐼 El bot se despide.` }, { quoted: m });
+    await conn.groupLeave(groupId);
+  } catch (error) {
+    console.error(error);
+    m.reply('🕸 No se pudo abandonar el grupo. Intenta nuevamente.');
+  }
+};
+
+handler.help = ['leave'];
+handler.tags = ['socket'];
+handler.command = ['leave'];
+
+export default handler;
