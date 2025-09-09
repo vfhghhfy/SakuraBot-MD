08c10749dfac134bd19d44bc18e0946d2a46b56f
Ell Contrerax <argumedodie@gmail.com>
2025-09-09 11:45:18 -0500

    Crear owner-restart.js

---
+++ plugins/owner-restart.js
@@ -0,0 +1,13 @@
+let handler = async (m, { conn }) => {
+  await conn.reply(m.chat, `🌾 Reiniciando el Socket...\n> 🐼 *Espere un momento...*`, m);
+  setTimeout(() => {
+    process.exit(0);
+  }, 3000);
+};
+
+handler.help = ['restart'];
+handler.tags = ['owner'];
+handler.command = ['restart'];
+handler.owner = true;
+
+export default handler;
