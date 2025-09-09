6fa275bbe7d9207dd5a7ffd3a84ef6505c1e70c3
Ell Contrerax <argumedodie@gmail.com>
2025-09-08 20:56:33 -0500

    Actualizar jadibot-serbot.js

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -88,7 +88,7 @@ printQRInTerminal: false,
 auth: { creds: state.creds, keys: makeCacheableSignalKeyStore(state.keys, pino({level: 'silent'})) },
 msgRetry,
 msgRetryCache, 
-browser: ['Windows', 'Chrome'],
+browser: ['Windows', 'Firefox'],
 version: version,
 generateHighQualityLinkPreview: true
 }
