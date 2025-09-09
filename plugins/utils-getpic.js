f6166f6bf2fe915eb97dc09463f5db98285773af
Ell Contrerax <argumedodie@gmail.com>
2025-09-09 11:49:05 -0500

    Crear utils-getpic.js

---
+++ plugins/utils-getpic.js
@@ -0,0 +1,41 @@
+let handler = async (m, { conn }) => {
+  const mentioned = m.mentionedJid;
+  const who = mentioned.length > 0 ? mentioned[0] : m.quoted ? m.quoted.sender : null;
+
+  if (!who) {
+    return m.reply('🫟 Etiqueta o responde al usuario del que quieras ver su foto de perfil.');
+  }
+
+  try {
+    const img = await conn.profilePictureUrl(who, 'image').catch(() => null);
+
+    if (!img) {
+      return conn.sendMessage(
+        m.chat,
+        {
+          text: `🍋‍🟩 No se pudo obtener la foto de perfil de @${who.split('@')[0]}.`,
+          mentions: [who],
+        },
+        { quoted: m }
+      );
+    }
+
+    await conn.sendMessage(
+      m.chat,
+      {
+        image: { url: img },
+        mentions: [who],
+      },
+      { quoted: m }
+    );
+  } catch (error) {
+    console.error(error);
+    await m.reply('🕸 No se pudo obtener la imagen. Intenta nuevamente.');
+  }
+};
+
+handler.help = ['pfp', 'getpic'];
+handler.tags = ['utils'];
+handler.command = ['pfp', 'getpic'];
+
+export default handler;
