5ee8d4452cd559f85018330cb48cdacc0be8780a
Ell Contrerax <argumedodie@gmail.com>
2025-09-09 12:08:09 -0500

    Crear group-demote.js

---
+++ plugins/group-demote.js
@@ -0,0 +1,36 @@
+const handler = async (m, { conn }) => {
+  try {
+    // let who = m?.message?.extendedTextMessage?.contextInfo?.participant || m?.mentionedJid[0] || await m?.quoted?.sender;
+   let texto = await m.mentionedJid
+   let who = texto.length > 0 ? texto[0] : (m.quoted ? await m.quoted.sender : false)
+    if (!who) return m.reply('🌾 Menciona al usuario que deseas degradar de administrador.');
+
+    const groupMetadata = await conn.groupMetadata(m.chat);
+    const participant = groupMetadata.participants.find(participant => participant.id === who);
+
+    if (!participant || !participant.admin) {
+    return conn.reply(m.chat, `🌾 *@${who.split('@')[0]}* no es administrador del grupo!`, m, { mentions: [who] });
+    }
+
+    if (who === groupMetadata.owner) {
+      return m.reply('⭐ No puedes degradar al creador del grupo de administrador.');
+    }
+
+    if (who === conn.user.jid) {
+      return m.reply('⭐ No puedes degradar al bot de administrador.');
+    }
+
+    await conn.groupParticipantsUpdate(m.chat, [who], 'demote');
+    await conn.reply(m.chat, `🕸 *@${who.split('@')[0]}* ha sido degradado de administrador del grupo!`, m, { mentions: [who] });
+  } catch (e) {
+    await m.reply(`🐼 Error.`);
+  }
+};
+
+handler.help = ['demote'];
+handler.tags = ['grupo'];
+handler.command = ['demote'];
+handler.admin = true;
+handler.botAdmin = true;
+
+export default handler;
