ef994c2cfe4f9a0645ec395a2d0632c42fb1ab25
Ell Contrerax <argumedodie@gmail.com>
2025-09-11 10:36:57 -0500

    Actualizar settings.js

--- settings.js
+++ settings.js
@@ -4,9 +4,7 @@ import { fileURLToPath } from "url"
 
 global.botNumber = ""
 
-global.owner = [
-  ["5492916450307", "Owner 👑", true]
-]
+global.owner = ["5492916450307"]
 
 global.botname = '🕸 SakuraBot-MD'
 global.namebot = '🥗 Sakura Bot'
