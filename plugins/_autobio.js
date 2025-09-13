290708048acc69bfc64333364ebac479e79502ac
Ell Contrerax <sarmientodieg@gmail.com>
2025-09-13 10:29:58 -0500

    Actualizar _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -8,7 +8,7 @@ if (process.send) { process.send('uptime')
 _muptime = await new Promise(resolve => { process.once('message', resolve) 
 setTimeout(resolve, 2000) }) * 1000}
 let uptime = clockString(_uptime)
-let bio = `${global.packname} ║ 🫛 ${uptime} ⌛ ║ 𓃠 ${["/menu", "/bots", "/code", "/qr", "Powered By » Night Light 🫗"].getRandom()}`
+let bio = `${global.packname} | 🫛 Uptime » ${uptime}`
 await this.updateProfileStatus(bio).catch(_ => _)
 // await this.updateProfilePicture(icon).catch(_ => _)
 setting.status = new Date() * 1
